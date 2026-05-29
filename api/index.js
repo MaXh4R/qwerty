d680e740e21254af17c73cc4ec11f9b74214b2fe
MaXh4R <muhiqbalalghifari@gmail.com>
2026-05-29 20:59:22 +0700

    testing7

--- api/index.js
+++ api/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const path = require("path")
-dotenv.config();
+require("dotenv").config();
 const { createClient } = require("@supabase/supabase-js");
 
 const app = express();
